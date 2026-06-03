9f43ce46945567290419c99c826a956fdacd5dce
Gavarla Rakesh <gavarlarakesh@gmail.com>
2026-06-03 18:35:47 +0530

    Update AllLowercaseCharacterPresence.c

--- AllLowercaseCharacterPresence.c
+++ AllLowercaseCharacterPresence.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() {
+    char st[1000001];
+    scanf("%s",st);
+    int flag=0,fl=0;
+    for(int i=0;st[i];i++){
+        flag=flag|(1<<(st[i]-'a'));
+    }
+    if(flag==(1<<26)-1)
+    printf("Yes");
+    else
+    printf("No");
+    return 0;
+}
