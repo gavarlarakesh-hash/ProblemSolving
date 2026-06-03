73d61a715fb2f52d3f986cc49ad0d7bae9dd0c22
Gavarla Rakesh <gavarlarakesh@gmail.com>
2026-06-03 18:38:31 +0530

    Create AllCaseCharacterPresence.c

---
+++ AllCaseCharacterPresence.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    char st[1000001];
+    scanf("%s",st);
+    int flag=0,fl=0;
+    for(int i=0;st[i];i++){
+        if(st[i]>='a' && st[i]<='z')
+        flag=flag|(1<<(st[i]-'a'));
+        else
+        fl=fl|(1<<(st[i]-'A'));
+    }
+    if((flag==(1<<26)-1)&&(fl==(1<<26)-1))
+    printf("Yes");
+    else
+    printf("No");
+    return 0;
+}
